6aace28c646a959cb066c407d47f5e353a06c91d
Selman <mohamedsalman.siddiq@philips.com>
2023-03-19 16:59:29 +0530

    Intro to Fragments and statically adding the fragment

--- app/src/main/java/com/tutorials/selman_fragments/ui/MainActivity.kt
+++ app/src/main/java/com/tutorials/selman_fragments/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tutorials.selman_fragments
+package com.tutorials.selman_fragments.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tutorials.selman_fragments.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
