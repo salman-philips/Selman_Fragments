cf54f2a75ce003390234fe29bcf755051fd5caa1
Selman <mohamedsalman.siddiq@philips.com>
2023-03-19 17:17:01 +0530

    Dynamically adding the fragment

--- app/src/main/java/com/tutorials/selman_fragments/ui/MainActivity.kt
+++ app/src/main/java/com/tutorials/selman_fragments/ui/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.tutorials.selman_fragments.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.tutorials.selman_fragments.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment()
+    }
+
+    private fun addFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val sampleFragment = SampleFragment()
+        fragmentTransaction.add(R.id.fragment_container, sampleFragment).commit()
     }
 }
